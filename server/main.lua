91011c6bcdd9ec44a6e17263ebc600a705583f70
Re1ease <86981407+Re1ease@users.noreply.github.com>
2022-02-22 19:06:44 +0100

    Update main.lua
    Server sync

--- server/main.lua
+++ server/main.lua
@@ -1,4 +1,4 @@
 RegisterNetEvent("pl-traffichack:server:changeLights")
-AddEventHandler("pl-traffichack:server:changeLights", function()
-	TriggerClientEvent("pl-traffichack:changeLights", -1)
+AddEventHandler("pl-traffichack:server:changeLights", function(obj)
+	TriggerClientEvent("pl-traffichack:changeLights", -1, obj)
 end)
